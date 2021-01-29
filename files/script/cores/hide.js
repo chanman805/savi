6b54b9fe97200dd8d9134da4eabf2e70ce51bee9
TheChanMan <69401710+chanman805@users.noreply.github.com>
2021-01-29 11:49:45 -0800

    Add files via upload

--- files/script/cores/hide.js
+++ files/script/cores/hide.js
@@ -1,9 +1,7 @@
 var unhide = function(){
 
 face.style= "transition:all 0.5s;position:fixed;top:10%;left:40%;";
-alert(face.style.left);
-vis4.style.transition = "all 2.5s";
-vis4.style.left = "-200%";
+
 
 wifiapp.style.transition = "all 1s";
 locationapp.style.transition = "all 1s";
@@ -22,9 +20,7 @@ face.style.transition = "all 1.5s";
 face.style.transitionDelay = "0.5s";
 face.style.left = "10%";
 
-vis4.style.transition = "all 1.5s";
-vis4.style.transitionDelay = "0.5s";
-vis4.style.left = "-15%";
+
 
 wifiapp.style.transition = "all 1s";
 locationapp.style.transition = "all 1s";
@@ -35,5 +31,8 @@ wifiapp.style.top = "150%";
 locationapp.style.top = "180%";
 timeapp.style.top = "210%";
 systemapp.style.top = "240%";
+
+
+
 
 };
