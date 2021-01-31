9280478b0fad3ed67bf519a67b70c83e07b4f16d
TheChanMan <69401710+chanman805@users.noreply.github.com>
2021-01-31 11:02:48 -0800

    Add files via upload

--- files/script/cores/app_library.js
+++ files/script/cores/app_library.js
@@ -1,6 +1,6 @@
 var alltheapps = function(){
 	backbutton.style.transition = "all 0.5s";
-	backbutton.style.display = "block";
+	backbutton.style.display = "inline";
 wifiapp.style.transition = "all 1s";
 locationapp.style.transition = "all 1s";
 timeapp.style.transition = "all 1s";
@@ -13,11 +13,11 @@ timeapp.style.top = "67.5%";
 systemapp.style.top = "45%";
 weatherapp.style.top = "0%";
 
-wifiapp.style.left= "77%";
-locationapp.style.left = "77%";
-timeapp.style.left = "77%";
-systemapp.style.left = "77%";
-weatherapp.style.left = "63%";
+wifiapp.style.left= "0%";
+locationapp.style.left = "0%";
+timeapp.style.left = "0%";
+systemapp.style.left = "0%";
+weatherapp.style.left = "20%";
 
 face.style.transition = "all 1s";
 face.style.top = "10%";
@@ -27,16 +27,6 @@ weatherapp.style.opacity = "98%";
 var normal = function(){
 		backbutton.style.transition = "all 0.5s";
 	backbutton.style.display = "none";
-
-wifiapp.style.top = "0%";
-locationapp.style.top = "22.5%";
-timeapp.style.top = "67.5%";
-systemapp.style.top = "45%";
-
-wifiapp.style.left= "0%";
-locationapp.style.left = "0%";
-timeapp.style.left = "0%";
-systemapp.style.left = "0%";
 face.style.transition = "all 1s";
 face.style.top = "10%";
 face.style.opacity = "100%";
